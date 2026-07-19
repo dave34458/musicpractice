db1b6d06733b6dc1a2b777f1337ff849d334bedc
vinson <vinsonjoshua56@yahoo.com>
2026-07-20 00:19:20 +0800

    feat: add playlists

--- backingtracks/urls.py
+++ backingtracks/urls.py
@@ -9,4 +9,12 @@ urlpatterns = [
     path('backingtracks/<int:track_id>/status/', views.track_status, name='track_status'),
     path('backingtracks/<int:track_id>/edit/', views.edit_track, name='track_edit'),
     path('backingtracks/<int:track_id>/delete/', views.delete_track, name='delete_track'),
+    path('backingtracks/<int:track_id>/add-to-playlist/', views.add_to_playlist, name='add_to_playlist'),
+    path('playlists/add-track/', views.add_to_playlist, name='playlist_add_track'),
+    path('backingtracks/<int:track_id>/remove-from-playlist/<int:playlist_id>/', views.remove_from_playlist, name='remove_from_playlist'),
+    path('playlists/', views.playlists, name='playlists'),
+    path('playlists/new/', views.create_playlist, name='create_playlist'),
+    path('playlists/<int:playlist_id>/', views.playlist_detail, name='playlist_detail'),
+    path('playlists/<int:playlist_id>/delete/', views.delete_playlist, name='delete_playlist'),
+    path('playlists/<int:playlist_id>/edit/', views.edit_playlist, name='edit_playlist'),
 ]
